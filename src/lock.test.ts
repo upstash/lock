a37eb692d8a4001ffd8309edb80f776ae56eafdb
Meshan Khosla <meshan@upstash.com>
2023-11-18 12:44:39 -0800

    fmt

--- src/lock.test.ts
+++ src/lock.test.ts
@@ -3,102 +3,102 @@ import { Redis } from "@upstash/redis";
 import { Lock } from "./lock";
 
 function getUniqueLockId() {
-	return `lock-test-${Math.random().toString(36).substr(2, 9)}`;
+  return `lock-test-${Math.random().toString(36).substr(2, 9)}`;
 }
 
 test("lock created, extended, and released with defaults", async () => {
-	const uniqueId = getUniqueLockId();
-	const lock = new Lock({
-		id: uniqueId,
-		redis: Redis.fromEnv(),
-	});
+  const uniqueId = getUniqueLockId();
+  const lock = new Lock({
+    id: uniqueId,
+    redis: Redis.fromEnv(),
+  });
 
-	expect(lock.id).toBe(uniqueId);
-	expect(await lock.getStatus()).toBe("FREE");
-	const acquired = await lock.acquire();
-	expect(acquired).toBe(true);
-	const extended = await lock.extend(10000);
-	expect(extended).toBe(true);
-	const released = await lock.release();
-	expect(released).toBe(true);
-	expect(await lock.getStatus()).toBe("FREE");
+  expect(lock.id).toBe(uniqueId);
+  expect(await lock.getStatus()).toBe("FREE");
+  const acquired = await lock.acquire();
+  expect(acquired).toBe(true);
+  const extended = await lock.extend(10000);
+  expect(extended).toBe(true);
+  const released = await lock.release();
+  expect(released).toBe(true);
+  expect(await lock.getStatus()).toBe("FREE");
 });
 
 test("lock created, extended, and released with values", async () => {
-	const uniqueId = getUniqueLockId();
-	const lock = new Lock({
-		id: uniqueId,
-		redis: Redis.fromEnv(),
-		lease: 5000,
-		retry: {
-			attempts: 1,
-			delay: 100,
-		},
-	});
+  const uniqueId = getUniqueLockId();
+  const lock = new Lock({
+    id: uniqueId,
+    redis: Redis.fromEnv(),
+    lease: 5000,
+    retry: {
+      attempts: 1,
+      delay: 100,
+    },
+  });
 
-	expect(lock.id).toBe(uniqueId);
-	expect(await lock.getStatus()).toBe("FREE");
-	const acquired = await lock.acquire();
-	expect(acquired).toBe(true);
-	const extended = await lock.extend(10000);
-	expect(extended).toBe(true);
-	const released = await lock.release();
-	expect(released).toBe(true);
-	expect(await lock.getStatus()).toBe("FREE");
+  expect(lock.id).toBe(uniqueId);
+  expect(await lock.getStatus()).toBe("FREE");
+  const acquired = await lock.acquire();
+  expect(acquired).toBe(true);
+  const extended = await lock.extend(10000);
+  expect(extended).toBe(true);
+  const released = await lock.release();
+  expect(released).toBe(true);
+  expect(await lock.getStatus()).toBe("FREE");
 });
 
 test("double lock acquisition fails", async () => {
-	const uniqueId = getUniqueLockId();
-	const lock = new Lock({
-		id: uniqueId,
-		redis: Redis.fromEnv(),
-		lease: 5000,
-		retry: {
-			attempts: 1,
-			delay: 100,
-		},
-	});
+  const uniqueId = getUniqueLockId();
+  const lock = new Lock({
+    id: uniqueId,
+    redis: Redis.fromEnv(),
+    lease: 5000,
+    retry: {
+      attempts: 1,
+      delay: 100,
+    },
+  });
 
-	expect(await lock.acquire()).toBe(true);
+  expect(await lock.acquire()).toBe(true);
 
-	// Attempt to acquire the same lock, which should fail
-	expect(await lock.acquire()).toBe(false);
+  // Attempt to acquire the same lock, which should fail
+  expect(await lock.acquire()).toBe(false);
 });
 
 test("lock acquisition fails by other instance", async () => {
-	const uniqueId = getUniqueLockId();
-	const lock = new Lock({
-		id: uniqueId,
-		redis: Redis.fromEnv(),
-	});
+  const uniqueId = getUniqueLockId();
+  const lock = new Lock({
+    id: uniqueId,
+    redis: Redis.fromEnv(),
+  });
 
-	expect(await lock.acquire()).toBe(true);
+  expect(await lock.acquire()).toBe(true);
 
-	// Attempt to acquire the same lock using a different lock instance, which should fail
-	const lockFail = new Lock({
-		id: uniqueId,
-		redis: Redis.fromEnv(),
-	});
+  // Attempt to acquire the same lock using a different lock instance, which should fail
+  const lockFail = new Lock({
+    id: uniqueId,
+    redis: Redis.fromEnv(),
+  });
 
-	expect(await lockFail.acquire()).toBe(false);
+  expect(await lockFail.acquire()).toBe(false);
 });
 
 test("lock lease times out", async () => {
-	const uniqueId = getUniqueLockId();
-	const lock = new Lock({
-		id: uniqueId,
-		redis: Redis.fromEnv(),
-		lease: 100,
-		retry: {
-			attempts: 1,
-			delay: 100,
-		},
-	});
+  const uniqueId = getUniqueLockId();
+  const lock = new Lock({
+    id: uniqueId,
+    redis: Redis.fromEnv(),
+    lease: 100,
+    retry: {
+      attempts: 1,
+      delay: 100,
+    },
+  });
 
-	expect(await lock.acquire()).toBe(true);
+  expect(await lock.acquire()).toBe(true);
 
-	// Wait for the lock to expire
-	await new Promise((resolve) => setTimeout(resolve, 200));
+  // Wait for the lock to expire
+  await new Promise((resolve) => setTimeout(resolve, 200));
 
-	expect(await lock.getStatus()).toBe("FREE");
+  expect(await lock.getStatus()).toBe("FREE");
 });
--- src/lock.ts
+++ src/lock.ts
@@ -2,70 +2,70 @@ import { randomUUID } from "crypto";
 import type { LockAcquireConfig, LockConfig, LockCreateConfig, LockStatus } from "./types";
 
 export class Lock {
-	private readonly config: LockConfig;
-	private readonly DEFAULT_LEASE_MS = 10000;
-	private readonly DEFAULT_RETRY_ATTEMPTS = 3;
-	private readonly DEFAULT_RETRY_DELAY_MS = 100;
+  private readonly config: LockConfig;
+  private readonly DEFAULT_LEASE_MS = 10000;
+  private readonly DEFAULT_RETRY_ATTEMPTS = 3;
+  private readonly DEFAULT_RETRY_DELAY_MS = 100;
 
-	constructor(config: LockCreateConfig) {
-		this.config = {
-			redis: config.redis,
-			id: config.id,
-			lease: config.lease ?? this.DEFAULT_LEASE_MS,
-			UUID: null, // set when lock is acquired
-			retry: {
-				attempts: config.retry?.attempts ?? this.DEFAULT_RETRY_ATTEMPTS,
-				delay: config.retry?.delay ?? this.DEFAULT_RETRY_DELAY_MS,
-			},
-		};
-	}
+  constructor(config: LockCreateConfig) {
+    this.config = {
+      redis: config.redis,
+      id: config.id,
+      lease: config.lease ?? this.DEFAULT_LEASE_MS,
+      UUID: null, // set when lock is acquired
+      retry: {
+        attempts: config.retry?.attempts ?? this.DEFAULT_RETRY_ATTEMPTS,
+        delay: config.retry?.delay ?? this.DEFAULT_RETRY_DELAY_MS,
+      },
+    };
+  }
 
-	/**
-	 * Tries to acquire a lock with the given configuration.
-	 * If successful, the `status` of the lock will be set to "ACQUIRED".
-	 * If unsuccessful, the method will retry based on the provided retry configuration.
-	 *
-	 * @param config - Configuration for acquiring the lock, including lease, retry attempts, and delay.
-	 */
-	public async acquire(acquireConfig?: LockAcquireConfig): Promise<boolean> {
-		// Allow for overriding the constructor lease and retry config
-		const lease = acquireConfig?.lease ?? this.config.lease;
-		const retryAttempts = acquireConfig?.retry?.attempts ?? this.config.retry?.attempts;
-		const retryDelay = acquireConfig?.retry?.delay ?? this.config.retry?.delay;
+  /**
+   * Tries to acquire a lock with the given configuration.
+   * If successful, the `status` of the lock will be set to "ACQUIRED".
+   * If unsuccessful, the method will retry based on the provided retry configuration.
+   *
+   * @param config - Configuration for acquiring the lock, including lease, retry attempts, and delay.
+   */
+  public async acquire(acquireConfig?: LockAcquireConfig): Promise<boolean> {
+    // Allow for overriding the constructor lease and retry config
+    const lease = acquireConfig?.lease ?? this.config.lease;
+    const retryAttempts = acquireConfig?.retry?.attempts ?? this.config.retry?.attempts;
+    const retryDelay = acquireConfig?.retry?.delay ?? this.config.retry?.delay;
 
-		let attempts = 0;
-		const UUID = randomUUID();
-		while (attempts < retryAttempts) {
-			const upstashResult = await this.config.redis.set(this.config.id, UUID, {
-				nx: true,
-				px: lease,
-			});
+    let attempts = 0;
+    const UUID = randomUUID();
+    while (attempts < retryAttempts) {
+      const upstashResult = await this.config.redis.set(this.config.id, UUID, {
+        nx: true,
+        px: lease,
+      });
 
-			if (upstashResult === "OK") {
-				this.config.UUID = UUID;
-				return true;
-			}
+      if (upstashResult === "OK") {
+        this.config.UUID = UUID;
+        return true;
+      }
 
-			attempts += 1;
+      attempts += 1;
 
-			// Wait for the specified delay before retrying
-			await new Promise((resolve) => setTimeout(resolve, retryDelay));
-		}
+      // Wait for the specified delay before retrying
+      await new Promise((resolve) => setTimeout(resolve, retryDelay));
+    }
 
-		// Lock acquisition failed
-		this.config.lease = lease;
-		this.config.UUID = null;
-		return false;
-	}
+    // Lock acquisition failed
+    this.config.lease = lease;
+    this.config.UUID = null;
+    return false;
+  }
 
-	/**
-	 * Safely releases the lock ensuring the UUID matches.
-	 * This operation utilizes a Lua script to interact with Redis and
-	 * guarantees atomicity of the unlock operation.
-	 * @returns {Promise<boolean>} True if the lock was released, otherwise false.
-	 */
-	public async release(): Promise<boolean> {
-		const script = `
+  /**
+   * Safely releases the lock ensuring the UUID matches.
+   * This operation utilizes a Lua script to interact with Redis and
+   * guarantees atomicity of the unlock operation.
+   * @returns {Promise<boolean>} True if the lock was released, otherwise false.
+   */
+  public async release(): Promise<boolean> {
+    const script = `
       -- Check if the current UUID still holds the lock
       if redis.call("get", KEYS[1]) == ARGV[1] then
         return redis.call("del", KEYS[1])
@@ -74,17 +74,17 @@ export class Lock {
       end
      `;
 
-		const numReleased = await this.config.redis.eval(script, [this.config.id], [this.config.UUID]);
-		return numReleased === 1;
-	}
+    const numReleased = await this.config.redis.eval(script, [this.config.id], [this.config.UUID]);
+    return numReleased === 1;
+  }
 
-	/**
-	 * Extends the duration for which the lock is held by a given amount of milliseconds.
-	 * @param amt - The number of milliseconds by which the lock duration should be extended.
-	 * @returns {Promise<boolean>} True if the lock duration was extended, otherwise false.
-	 */
-	public async extend(amt: number): Promise<boolean> {
-		const script = `
+  /**
+   * Extends the duration for which the lock is held by a given amount of milliseconds.
+   * @param amt - The number of milliseconds by which the lock duration should be extended.
+   * @returns {Promise<boolean>} True if the lock duration was extended, otherwise false.
+   */
+  public async extend(amt: number): Promise<boolean> {
+    const script = `
       -- Check if the current UUID still holds the lock
       if redis.call("get", KEYS[1]) ~= ARGV[1] then
         return 0
@@ -99,37 +99,37 @@ export class Lock {
       end
      `;
 
-		const extendBy = amt / 1000; // convert to seconds
-		const extended = await this.config.redis.eval(
-			script,
-			[this.config.id],
-			[this.config.UUID, extendBy],
-		);
+    const extendBy = amt / 1000; // convert to seconds
+    const extended = await this.config.redis.eval(
+      script,
+      [this.config.id],
+      [this.config.UUID, extendBy],
+    );
 
-		if (extended === 1) {
-			this.config.lease += amt;
-		}
-		return extended === 1;
-	}
+    if (extended === 1) {
+      this.config.lease += amt;
+    }
+    return extended === 1;
+  }
 
-	get id(): string {
-		return this.config.id;
-	}
+  get id(): string {
+    return this.config.id;
+  }
 
-	/**
-	 * Gets the status of the lock, ie: ACQUIRED or FREE.
-	 * @returns {Promise<LockStatus>} The status of the lock.
-	 */
-	async getStatus(): Promise<LockStatus> {
-		if (this.config.UUID === null) {
-			return "FREE";
-		}
+  /**
+   * Gets the status of the lock, ie: ACQUIRED or FREE.
+   * @returns {Promise<LockStatus>} The status of the lock.
+   */
+  async getStatus(): Promise<LockStatus> {
+    if (this.config.UUID === null) {
+      return "FREE";
+    }
 
-		const UUID = await this.config.redis.get(this.config.id);
-		if (UUID === this.config.UUID) {
-			return "ACQUIRED";
-		}
+    const UUID = await this.config.redis.get(this.config.id);
+    if (UUID === this.config.UUID) {
+      return "ACQUIRED";
+    }
 
-		return "FREE";
-	}
+    return "FREE";
+  }
 }
