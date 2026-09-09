1a423a42467d7e325607b08225c02a37da69a768
CahidArda <cahidardaooz@gmail.com>
2026-09-09 14:50:42 +0300

    fix: rm new lie before config

--- src/lock.ts
+++ src/lock.ts
@@ -130,8 +130,7 @@ export class Lock implements AsyncDisposable {
       return false;
     }
 
-    const script = `
-      #!lua flags=allow-key-locking
+    const script = `#!lua flags=allow-key-locking
       -- Check if the current UUID still holds the lock
       if redis.call("get", KEYS[1]) == ARGV[1] then
         return redis.call("del", KEYS[1])
@@ -157,8 +156,7 @@ export class Lock implements AsyncDisposable {
       return false;
     }
 
-    const script = `
-      #!lua flags=allow-key-locking
+    const script = `#!lua flags=allow-key-locking
       -- Check if the current UUID still holds the lock
       if redis.call("get", KEYS[1]) ~= ARGV[1] then
         return 0
